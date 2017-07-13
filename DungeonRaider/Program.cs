6bcb3664a6a9ed4a095620ff90ec7d095426a9d7
Claudio Balbin <cbalbin@gradualinvestimentos.com.br>
2017-07-13 15:15:48 -0300

    Including original files.

---
+++ DungeonRaider/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonRaider
+{
+    class Program
+    {
+        private const int MAX_Y = 10;
+        private const int MAX_X = 20;
+        public static string[,] stage;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Dungeon Raider";
+            Console.SetWindowSize(40, 20);
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Stages stg = new Stages();
+
+            //string[,] stage = new string[MAX_Y, MAX_X];
+            //stage = new string[MAX_Y, MAX_X];
+
+            stage = Stages.GetStage(1);
+
+            int heroPosX = 1;
+            int heroPosY = 1;
+
+            printStage(stage, heroPosX, heroPosY);
+
+            ConsoleKeyInfo cki;
+
+            do
+            {
+                cki = Console.ReadKey();
+                Console.Clear();
+
+                //if (cki.Key == ConsoleKey.RightArrow && heroPosX < MAX_X-2)
+                if (cki.Key == ConsoleKey.RightArrow && podeIrDireita(heroPosX, heroPosY))
+                    heroPosX++;
+
+                if (cki.Key == ConsoleKey.LeftArrow && podeIrEsquerda(heroPosX, heroPosY))
+                    heroPosX--;
+
+                if (cki.Key == ConsoleKey.UpArrow && podeIrCima(heroPosX, heroPosY))
+                    heroPosY--;
+
+                if (cki.Key == ConsoleKey.DownArrow && podeIrBaixo(heroPosX, heroPosY))
+                    heroPosY++;
+
+                stage = Stages.GetStage(1);
+
+                printStage(stage, heroPosX, heroPosY);
+
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+
+        public static void printStage(string[,] stage, int heroPosX, int heroPosY)
+        {
+            stage[heroPosY, heroPosX] = "@";
+
+            //Print all
+            cabecalho();
+            for (int i = 0; i < MAX_Y; i++)
+            {
+                Console.Write("          ");
+                for (int j = 0; j < MAX_X; j++)
+                {
+                    Console.Write(stage[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            rodape();
+        }
+
+        public static bool podeIrDireita(int heroPosX, int heroPosY)
+        {
+            string prox = stage[heroPosY, heroPosX + 1].ToString();
+
+            if (ehParede(prox)) //Valida se o proximo eh parede
+                return false;
+
+            return true;
+        }
+
+        public static bool podeIrEsquerda(int heroPosX, int heroPosY)
+        {
+            string prox = stage[heroPosY, heroPosX - 1].ToString();
+
+            if (ehParede(prox)) //Valida se o proximo eh parede
+                return false;
+
+            return true;
+        }
+
+        public static bool podeIrCima(int heroPosX, int heroPosY)
+        {
+            string prox = stage[heroPosY-1, heroPosX].ToString();
+
+            if (ehParede(prox)) //Valida se o proximo eh parede
+                return false;
+
+            Stages.abrePorta(heroPosX, heroPosY - 1, 1);
+
+            return true;
+        }
+
+        public static bool podeIrBaixo(int heroPosX, int heroPosY)
+        {
+            string prox = stage[heroPosY + 1, heroPosX].ToString();
+
+            if (ehParede(prox)) //Valida se o proximo eh parede
+                return false;
+
+            return true;
+        }
+
+        public static bool ehParede(string duvida)
+        {
+            if (duvida == "║" || duvida == "╣" || duvida == "╗" || duvida == "╝" || duvida == "╚" || duvida == "╔"
+                || duvida == "╩" || duvida == "╦" || duvida == "╠" || duvida == "═" || duvida == "╬" || duvida == "#")
+                return true;
+
+            return false;
+        }
+
+        private static void cabecalho()
+        {
+            Console.WriteLine();
+            Console.WriteLine("======== ░▒▓ DUNGEON RAIDER ▓▒░ ========");
+            Console.WriteLine();
+        }
+
+        private static void rodape()
+        {
+            Console.WriteLine();
+            string risca = "";
+            for (int i = 0; i < (MAX_X*2); i++)
+                risca = risca + "=";
+            Console.WriteLine(risca);
+            Console.WriteLine();
+            Console.WriteLine("Press ESC to exit...");
+        }
+    }
+}
---
+++ DungeonRaider/Stages.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace DungeonRaider
+{
+    public class Stages
+    {
+        public const int MAX_LINES = 10;
+        public const int MAX_ROWS = 20;
+
+        public static List<StringBuilder> stages = new List<StringBuilder>();
+
+        public Stages()
+        {
+            carregaFases();
+        }
+
+        private void carregaFases()
+        {
+            //http://www.theasciicode.com.ar/
+
+            StringBuilder stage1 = new StringBuilder();
+            stage1.AppendLine("╔═╦════════════════╗");
+            stage1.AppendLine("║ ║                ║");
+            stage1.AppendLine("║ ║ ║              ║");
+            stage1.AppendLine("║ ║ ║              ║");
+            stage1.AppendLine("║ ║#║              ║");
+            stage1.AppendLine("║   ║              ║");
+            stage1.AppendLine("╠═╗ ║              ║");
+            stage1.AppendLine("║x║ ║              ║");
+            stage1.AppendLine("║   ║              ║");
+            stage1.AppendLine("╚═══╩══════════════╝");
+
+            stages.Add(stage1);
+        }
+
+        public static string[,] GetStage(int v)
+        {
+
+            StringBuilder stg = stages[v-1];
+
+            string[] lines = stg.ToString().Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            string[,] stageX = new string[MAX_LINES, MAX_ROWS];
+
+            for (int i = 0; i < MAX_LINES; i++)
+            {
+                for (int j = 0; j < MAX_ROWS; j++)
+                {
+                    stageX[i, j] = lines[i][j].ToString();
+                }
+            }
+
+            return stageX;
+        }
+
+        public static void abrePorta(int heroPosX, int heroPosY, int currentStage)
+        {
+            if (currentStage == 1)
+            {
+                if (heroPosX == 1 && heroPosY == 7) //Porta1
+                {
+                    removePorta(1, 3, 4);
+                    var synth = new SpeechSynthesizer();
+                    synth.SpeakAsync("abriu a porta nesse caráiu!");
+                }
+            }
+        }
+
+        private static void removePorta(int currentStage, int porta1_x, int porta1_y)
+        {
+            //Remove porta
+            string[] stageLines = stages[currentStage - 1].ToString().Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            string linha = stageLines[porta1_y];
+            char[] linhaArray = linha.ToCharArray();
+            linhaArray[porta1_x] = ' ';
+            linha = new string(linhaArray);
+            stageLines[porta1_y] = linha;
+
+            stages[currentStage - 1] = new StringBuilder(string.Join(Environment.NewLine, stageLines));
+        }
+
+    }
+}
